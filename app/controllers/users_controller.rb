d02829dd1bb20d9d8881e14514e1fea61561d406
Gustavo Sobral <ghsobral@gmail.com>
2016-04-04 15:42:24 -0300

    Add User delete, password validation and fix navbar syntax

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -12,4 +12,11 @@ class UsersController < ApplicationController
     end
   end
 
+  def destroy
+    @user = User.find(params[:id])
+    @user.destroy
+
+    redirect_to users_path
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,5 +2,13 @@ class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
   devise :database_authenticatable, :registerable,
-         :recoverable, :rememberable, :trackable, :validatable
+         :recoverable, :rememberable, :trackable, :validatable, password_length: 8..12
+
+  validate :password_complexity
+
+  def password_complexity
+    if password.present? and not password.match(/^(?=.*[0-9]{2})(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/)
+      errors.add :password, 'must include at least one letter and two digits'
+    end
+  end
 end
