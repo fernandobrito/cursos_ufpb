634df0013b9423b78bc7c3bf617f7a0df97448e5
Fernando Brito <email@fernandobrito.com>
2016-04-30 21:35:00 -0300

    Add pie chart: courses groupped by situation

--- app/controllers/transcripts_controller.rb
+++ app/controllers/transcripts_controller.rb
@@ -10,23 +10,34 @@ class TranscriptsController < ApplicationController
       return
     end
 
-    results = parser.course_results
+    # Process data for line graph
+    @results = parser.course_results
 
-    semester_average = []
+    @semester_average = []
 
-    results.semesters[0..-2].each_with_index do |semester, index|
-      semester_average << [ {v: index, f: semester },
-                             results.average_up_to(semester),
-                             results.average_up_to(semester, approved_only: true)]
+    @results.semesters[0..-2].each_with_index do |semester, index|
+      @semester_average << [ {v: index, f: semester },
+                             @results.average_up_to(semester),
+                             @results.average_up_to(semester, approved_only: true)]
     end
 
-    gon.semesters_average = semester_average
+    gon.semesters_average = @semester_average
+
+    # Process data for pie graph
+    @courses_groupped_by_situation = @results.results.group_by(&:situation)
+
+    @courses_groupped_by_situation.map { |situation, courses| @courses_groupped_by_situation[situation] = courses.count }
+    @courses_groupped_by_situation = @courses_groupped_by_situation.sort_by { |_, count| count }.reverse
+
+    @total_courses = @results.results.size
+
+    gon.courses_groupped_by_situation = @courses_groupped_by_situation.to_a
 
     # Save file on Dropbox
     if ENV['RAILS_ENV'] == 'production'
       client = DropboxClient.new(ENV['DROPBOX_ACCESS_TOKEN'])
       filename = "#{results.student.id}_#{results.semesters.last.sub('.','_')}.pdf"
-      response = client.put_file(filename, params[:file].read)
+      client.put_file(filename, params[:file].read)
     end
 
     render :show
