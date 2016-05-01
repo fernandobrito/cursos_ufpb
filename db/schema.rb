709270d717b0c6971c3f82291490313ec10f8293
Fernando Brito <email@fernandobrito.com>
2016-05-01 20:15:18 -0300

    Use postgresql on localhost
    So it's easier to migrate local data do heroku

--- db/schema.rb
+++ db/schema.rb
@@ -13,6 +13,9 @@
 
 ActiveRecord::Schema.define(version: 20160501201101) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "programs", force: :cascade do |t|
     t.string   "name",       null: false
     t.datetime "created_at", null: false
@@ -27,8 +30,8 @@ ActiveRecord::Schema.define(version: 20160501201101) do
     t.datetime "updated_at",                            null: false
   end
 
-  add_index "students", ["code"], name: "index_students_on_code"
-  add_index "students", ["program_id"], name: "index_students_on_program_id"
+  add_index "students", ["code"], name: "index_students_on_code", using: :btree
+  add_index "students", ["program_id"], name: "index_students_on_program_id", using: :btree
 
   create_table "users", force: :cascade do |t|
     t.string   "email",                  default: "", null: false
@@ -46,7 +49,8 @@ ActiveRecord::Schema.define(version: 20160501201101) do
     t.string   "name"
   end
 
-  add_index "users", ["email"], name: "index_users_on_email", unique: true
-  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
+  add_index "users", ["email"], name: "index_users_on_email", unique: true, using: :btree
+  add_index "users", ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true, using: :btree
 
+  add_foreign_key "students", "programs"
 end
