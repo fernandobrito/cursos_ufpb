bddf22866a6beeeaf2cd1796992b5b7f2662dd62
Fernando Brito <email@fernandobrito.com>
2016-05-03 16:24:07 -0300

    Add validations to User and handle exception

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -1,22 +1,30 @@
 class Admin::UsersController < ApplicationController
   before_action :authenticate_user!
+  before_action :find_resource, only: [:show, :destroy]
 
   def index
     @users = User.all
   end
 
   def show
-    @user = User.find(params[:id])
-
     unless @user == current_user
       redirect_to :back, :alert => "Access denied."
     end
   end
 
   def destroy
-    @user = User.find(params[:id])
     @user.destroy
 
     redirect_to admin_users_path
+  end
+
+protected
+  def find_resource
+    begin
+      @user = User.find(params[:id])
+    rescue ActiveRecord::RecordNotFound
+      redirect_to admin_users_path, :alert => 'Usuário não encontrado.'
+      return
+    end
   end
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -5,6 +5,12 @@ class User < ActiveRecord::Base
          :recoverable, :rememberable, :trackable, :validatable, password_length: 8..12
 
   validate :password_complexity
+  validate :name_without_numbers
+  validates :name, length: { maximum: 20 }, presence: true
+
+  def name_without_numbers
+    errors.add :name, 'must not include numbers' if (name && ! name.match(/[0-9]/).nil?)
+  end
 
   def password_complexity
     if password.present? and not password.match(/^(?=.*[0-9]{2})(?=.*[a-zA-Z])([a-zA-Z0-9]+)$/)
