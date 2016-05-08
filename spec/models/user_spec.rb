f72e4ae63ebfa83920022971bce560fa221af674
Gustavo Sobral <ghsobral@gmail.com>
2016-05-08 00:38:22 -0300

    Refactor user spec to use new factory objects

--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,37 +1,37 @@
 describe User do
   context 'when has invalid attributes' do
     it 'should not accept without name' do
-      user = FactoryGirl.build(:user, name: '')
+      user = FactoryGirl.build(:user_name_blank)
       user.valid?
       expect(user.errors[:name]).to include("can't be blank")
     end
 
     it 'should not accept numbers in name' do
-      user = FactoryGirl.build(:user, name: 'Name 123')
+      user = FactoryGirl.build(:user_name_with_numbers)
       user.valid?
       expect(user.errors[:name]).to include('must not include numbers')
     end
 
     it 'should not accept password without at least 2 numbers' do
-      user = FactoryGirl.build(:user, password: 'changeme1')
+      user = FactoryGirl.build(:user_passwd_without_numbers)
       user.valid?
       expect(user.errors[:password].join(' ')).to match('letter and two digits')
     end
 
     it 'should not accept password without at least 1 letter' do
-      user = FactoryGirl.build(:user, password: '123456678')
+      user = FactoryGirl.build(:user_passwd_without_letters)
       user.valid?
       expect(user.errors[:password].join(' ')).to match('letter and two digits')
     end
 
     it 'should not accept password shorter than 8 chars' do
-      user = FactoryGirl.build(:user, password: 'pa12')
+      user = FactoryGirl.build(:user_passwd_short)
       user.valid?
       expect(user.errors[:password].join(' ')).to match('too short')
     end
 
     it 'should not accept password longer than 12 chars' do
-      user = FactoryGirl.build(:user, password: 'passwordpassword')
+      user = FactoryGirl.build(:user_passwd_long)
       user.valid?
       expect(user.errors[:password].join(' ')).to match('too long')
     end
