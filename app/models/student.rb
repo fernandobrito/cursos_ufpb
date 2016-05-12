c73967fce47a8c757914bf9cfbf826d57dc17f98
Gustavo Sobral <ghsobral@gmail.com>
2016-05-12 03:24:03 -0300

    Add average_grade student validation

--- app/models/student.rb
+++ app/models/student.rb
@@ -6,4 +6,5 @@ class Student < ActiveRecord::Base
 
   validates :code, presence: true, uniqueness: true
   validates :program, presence: true
+  validates :average_grade, allow_nil: true, numericality: { greater_than_or_equal_to: 0, less_than_or_equal_to: 10 }
 end
