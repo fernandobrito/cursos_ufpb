2d427db27e62b78486e79c5a04ea8124efd197d9
David Guilherme <davidlguilherme@gmail.com>
2016-05-16 19:50:44 -0300

    Specify color of the charts based by situation

---
+++ app/assets/javascripts/ChartUtils.js
@@ -0,0 +1,21 @@
+(function (expose) {
+    var ChartUtils = {};
+
+    ChartUtils.SITUATIONS_COLORS = {
+        'APROVADO': '#0000FF',
+        'REPROVADO': '#FF0000',
+        'MATRICULADO': '#FFFF00',
+        'TRANCADO': '#808080',
+        'DISPENSADO': '#00FF00'
+    };
+
+    ChartUtils.getSituationsColors = function (situations) {
+        var colors = [];
+        situations.forEach(function (situation) {
+           colors.push(ChartUtils.SITUATIONS_COLORS[situation]);
+        });
+        return colors;
+    };
+
+    expose.ChartUtils = ChartUtils;
+})(window);
