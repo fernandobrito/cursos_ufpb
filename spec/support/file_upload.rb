44f9b2869a3b8a6510f81482e8028bae9291e1e6
Gustavo Sobral <ghsobral@gmail.com>
2016-05-17 00:50:08 -0300

    Override Rack UploadFile class
    This way, is possible to keep the uploaded file extension when
    transformed in tempfile

---
+++ spec/support/file_upload.rb
@@ -0,0 +1,18 @@
+module Rack
+  module Test
+    class UploadedFile
+      def initialize(path, content_type = "text/plain", binary = false)
+        raise "#{path} file does not exist" unless ::File.exist?(path)
+
+        @content_type = content_type
+        @original_filename = ::File.basename(path)
+
+        @tempfile = Tempfile.new([@original_filename, ::File.extname(path)])
+        @tempfile.set_encoding(Encoding::BINARY) if @tempfile.respond_to?(:set_encoding)
+        @tempfile.binmode if binary
+
+        FileUtils.copy_file(path, @tempfile.path)
+      end
+    end
+  end
+end
