a9166b67da8e8c9d72509be6a69fe6b33feb9aa4
Fernando Brito <email@fernandobrito.com>
2016-05-22 08:52:57 -0300

    Change chart colors
    Now Trancado is gray, Matriculado is green and Aprovado is blue

--- app/assets/javascripts/ChartUtils.js
+++ app/assets/javascripts/ChartUtils.js
@@ -1,13 +1,14 @@
 (function (expose) {
     var ChartUtils = {};
 
+    // http://there4.io/2012/05/02/google-chart-color-list/
     ChartUtils.SITUATIONS_COLORS = {
-        'APROVADO': '#109618',
-        'REPROVADO': '#DC3912',
-        'MATRICULADO': '#3366CC',
-        'TRANCADO': '#FF9900',
-        'DISPENSADO': '#949BA2',
-        'REP. FALTA': '#8B0707'
+        'APROVADO': '#3366CC',    // Default blue
+        'REPROVADO': '#DC3912',   // Default red
+        'MATRICULADO': '#109618', // Green
+        'TRANCADO': '#949BA2',    // Gray
+        'DISPENSADO': '#0099C6',  // Light blue
+        'REP. FALTA': '#8B0707'   // Wine (dark red)
     };
 
     ChartUtils.getSituationsColors = function (situations) {
