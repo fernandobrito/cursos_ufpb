72110155d1e8513db9bd42a219ffa7eef55edc1f
Gustavo Sobral <ghsobral@gmail.com>
2016-06-28 12:48:44 -0300

    Fix student resubmition bug
    When student submit a new transcript with a new average grade, the
    ActiveRecord doesnt find the saved student on DB

--- app/controllers/transcripts_controller.rb
+++ app/controllers/transcripts_controller.rb
@@ -54,8 +54,10 @@ class TranscriptsController < ApplicationController
     average_grade = parser.course_results.average_up_to(parser.course_results.semesters.last)
 
     program = Program.find_or_create_by!(name: program_name)
-    program.students.find_or_create_by!(code: parsed_student.id,
-                                        average_grade: average_grade)
+    student = program.students.find_or_create_by!(code: parsed_student.id)
+
+    student.average_grade = average_grade
+    student.save!
 
     # Save file on Dropbox
     if ENV['RAILS_ENV'] == 'production' && !is_sample_file
